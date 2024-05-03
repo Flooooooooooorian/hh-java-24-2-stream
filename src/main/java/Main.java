a40de6bb17fdc75c70e483662ca66287edc6c5e9
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-05-03 14:06:29 +0200

    Add sum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,8 +40,8 @@ public class Main {
                 .filter(integer -> integer % 2 == 0)
                 .map(integer -> integer * 2)
                 .mapToInt(Integer::intValue)
-                .sorted());
-                //.sum()
+                .sorted()
+                .sum());
                 //.reduce(0, (a, b) -> a + b));
 
     }
